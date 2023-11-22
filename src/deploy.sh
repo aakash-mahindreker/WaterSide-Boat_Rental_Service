f2c6a67299fce27e6e63189b4eaafe1a7684db2b
Aakash Mahindreker <70765660+aakash-mahindreker@users.noreply.github.com>
2023-11-22 16:04:15 -0600

    deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,7 +2,5 @@ exec > >(tee -i deploy.log)
 exec 2>&1
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-
-/opt/render/project/src/.venv/bin/python -m pip install -r requirements.txt
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade python
 /opt/render/project/src/.venv/bin/python app.py
