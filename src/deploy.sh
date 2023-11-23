ba32c9ae9f6a36c30a897b457c17a14f846ed305
Aakash Mahindreker <70765660+aakash-mahindreker@users.noreply.github.com>
2023-11-22 18:29:07 -0600

    Delete src directory

--- src/deploy.sh
+++
@@ -1,6 +0,0 @@
-exec > >(tee -i deploy.log)
-exec 2>&1
-
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade python
-/opt/render/project/src/.venv/bin/python app.py
--- src/rec.py
+++
@@ -1,2 +0,0 @@
-x = "http://127.0.0.1:3030/reservations?button=cancel".split("?")
-print(x)
